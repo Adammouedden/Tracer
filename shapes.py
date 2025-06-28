906a39ac686d1071811ea12ac38b4e770173d2ef
Joshua Bandy <joshuatylerbandy@gmail.com>
2025-06-28 11:29:10 -0400

    finished draw_arrow_bidirect

--- shapes.py
+++ shapes.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 import configs as cfg
 
 pygame.init()
@@ -6,11 +7,11 @@ pygame.init()
 #Base Shapes
 def draw_text(surface, text: str, coordinates, font_size = 20, color = cfg.BLACK):
     font = pygame.font.SysFont("ubuntu", font_size)
-    text_surface = font.render(text, True, color)
+    # text_surface = font.render(text, True, color
     x, y = coordinates
     new_coordinates = (x, y + cfg.text_offset)
-    text_rectangle = text_surface.get_rect(topleft=(new_coordinates))
-    surface.blit(text_surface, text_rectangle)
+    text_rectangle = surface.get_rect(topleft=(new_coordinates))
+    surface.blit(surface, text_rectangle)
 
 def draw_rectangle(surface, coordinates, rectangle_width, rectangle_height, border=0, color= cfg.BLACK):
     rect_object = (coordinates[0], coordinates[1], rectangle_width, rectangle_width)
@@ -34,3 +35,30 @@ def draw_node(surface, value, coordinates, rectangle_width, rectangle_height, er
     draw_rectangle(surface, coordinates, rectangle_width, rectangle_height, 2, cfg.BLACK)
     draw_text(surface, f"{value}", (coordinates[0] + cfg.text_offset, coordinates[1] + cfg.text_offset))
 
+def draw_arrow_bidirect(surface, start_pos, end_pos, color = cfg.BLACK):
+    draw_line(surface, start_pos, end_pos, color)
+    
+    # Find arrow head points
+    angle1 = math.atan2((end_pos[1] - start_pos[1]),(end_pos[0] - start_pos[0]))
+    xa = start_pos[0] + (cfg.arrow_head_distance * math.cos(angle1 + math.pi/4))
+    ya = start_pos[0] + (cfg.arrow_head_distance * math.sin(angle1 + math.pi/4))
+    a_pos = (xa, ya)
+
+
+    xb = start_pos[0] + (cfg.arrow_head_distance * math.cos(angle1 + math.pi/4))
+    yb = start_pos[0] + (cfg.arrow_head_distance * math.sin(angle1 + math.pi/4))
+    b_pos = (xb, yb)
+
+    angle2 = math.atan2((start_pos[1]-end_pos[1]),(start_pos[0]-end_pos[0]))
+    xc = start_pos[0] + (cfg.arrow_head_distance * math.cos(angle2 + math.pi/4))
+    yc = start_pos[0] + (cfg.arrow_head_distance * math.sin(angle2 + math.pi/4))
+    c_pos = (xc, yc)
+
+    xd = start_pos[0] + (cfg.arrow_head_distance * math.cos(angle2 + math.pi/4))
+    yd = start_pos[0] + (cfg.arrow_head_distance * math.sin(angle2 + math.pi/4))
+    d_pos = (xd, yd)
+
+    draw_line(surface, start_pos, a_pos, color)
+    draw_line(surface, start_pos, b_pos, color)
+    draw_line(surface, start_pos, c_pos, color)
+    draw_line(surface, start_pos, d_pos, color)

b2d4a326124a25a249cfab7e9106fbe402aac475
Adam Mouedden <adam.mouedden@gmail.com>
2025-06-28 13:58:21 -0400

    Fixed a bug where the model only ever called 1 function

--- viz_window.py
+++ viz_window.py
@@ -7,7 +7,6 @@ pygame.init()
 def create_viz_window(surface_color):
     viz_window_surface = pygame.Surface((cfg.VIZ_WINDOW_WIDTH, cfg.VIZ_WINDOW_HEIGHT))
     viz_window_surface.fill(surface_color)
-    shapes.draw_line(viz_window_surface, (cfg.VIZ_WINDOW_WIDTH//2, 0), (cfg.VIZ_WINDOW_WIDTH//2, cfg.VIZ_WINDOW_HEIGHT))
     return viz_window_surface
 
 def reset(surface, surface_color):

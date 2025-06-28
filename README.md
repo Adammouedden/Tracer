ba3fb316a0dba5580062dc22cf796b1b9d5d831f
Joshua Bandy <joshuatylerbandy@gmail.com>
2025-06-28 10:30:46 -0400

    Started shapes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Tracer
+
+
+# Needed pip installs
+```
+pip install pygame
+pip install google-genai
+```
--- shapes.py
+++ shapes.py
@@ -1,4 +1,21 @@
 import pygame
 
-#Base Shapes
 
+pygame.init()
+
+
+text_offset = 30
+
+#Base Shapes
+def draw_text(surface, text: str, coordinates, font_size = 20, color = BLACK):
+    font = pygame.font.SysFont("ubuntu", font_size)
+    text_surface = font.render(text, True, color)
+    x, y = coordinates
+    new_coordinates = (x, y + text_offset)
+    text_rectangle = text_surface.get_rect(topleft=(new_coordinates))
+    surface.blit(text_surface, text_rectangle)
+
+def draw_rectangle(surface, coordinates, rectangle_width, rectangle_height, border=0, color=BLACK):
+
+def draw_line(surface, start_pos, end_pos, color=BLACK):
+    pygame.draw.line(surface)
